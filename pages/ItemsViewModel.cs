721f1b89f8ecb3388723cef356d1b989374b81fa
Vdovenko Dmitry <vdovenko.dmitry@mail.ru>
2024-03-22 14:18:29 +0300

    create scrollable items and start creating logic of safe items

---
+++ pages/ItemsViewModel.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassSafe.pages
+{
+    public class ItemsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<PassItem> passItems;
+
+        public ObservableCollection<PassItem> PassItems
+        {
+            get { return passItems; }
+            set
+            {
+                passItems = value;
+                OnPropertyChanged(nameof(PassItems));
+            }
+        }
+
+        public ItemsViewModel()
+        {
+            PassItems = new ObservableCollection<PassItem>();
+            PassItems.Add(new PassItem {
+                Title = "Telegram", 
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem {
+                Title = "Mail", 
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Telegram",
+                ImageSource = "/Images/telegram.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+            PassItems.Add(new PassItem
+            {
+                Title = "Mail",
+                ImageSource = "/Images/email.png",
+                Password = "1234",
+                Login = "Huggy"
+            });
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class PassItem
+    {
+        public string ImageSource { get; set; }
+        public string Title { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
+
